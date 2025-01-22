be7bed112325e3a5ec7333adb37835b345540c2d
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2025-01-22 13:15:21 +0200

    refactor

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,19 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:movie_app/core/data_sources/networking/api_service.dart';
 import 'package:movie_app/features/auth/presentation/controller/auth/auth_cubit.dart';
 import 'package:movie_app/features/auth/presentation/controller/login/login_cubit.dart';
 import 'package:movie_app/features/auth/presentation/controller/signup/signup_cubit.dart';
-import 'package:movie_app/features/home/presentation/views/home_screen.dart';
+import 'package:movie_app/features/home/presentation/views/screen/layout_screen.dart';
 import 'package:movie_app/features/onboarding/presentation/views/onboarding_screen.dart';
 
 import '../../features/auth/data/repositories/auth_repo_impl.dart';
 import '../../features/auth/presentation/views/email_verificationScreen.dart';
 import '../../features/auth/presentation/views/login_screen.dart';
 import '../../features/auth/presentation/views/signup_screen.dart';
+import '../../features/home/data/repositories/trending_movie/trending_movies_repo_impl.dart';
+import '../../features/home/presentation/controller/trending/trending_cubit.dart';
 import '../../features/splash/presentation/views/splash_screen.dart';
-import '../networking/firebase_service.dart';
+import '../data_sources/networking/firebase_service.dart';
 
 abstract class AppRoutes {
   static const kInitialRoute = '/';
@@ -75,7 +79,16 @@ abstract class AppRoutes {
       ),
       GoRoute(
         path: kHomeRoute,
-        builder: (context, state) => HomeScreen(),
+        builder: (context, state) => MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (context) =>
+                  TrendingCubit(TrendingMoviesRepoImpl(ApiService(Dio())))
+                    ..fetchTrendingMoviesByDay(),
+            ),
+          ],
+          child: LayoutScreen(),
+        ),
       ),
     ],
   );
