0777f8f15710666b74436141269e71c47bacb8b4
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2025-01-22 13:10:44 +0200

    refactor

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,49 +1,4 @@
-import 'package:firebase_core/firebase_core.dart';
-
-import '../../generated/l10n.dart';
-
 abstract class Failure {
   final String message;
   const Failure(this.message);
-}
-
-class ServerFailure extends Failure {
-  const ServerFailure(super.message);
-
-  factory ServerFailure.fromFirebaseException(dynamic e) {
-    String errorMessage = _getErrorMessage(e);
-    return ServerFailure(errorMessage);
-  }
-
-  static String _getErrorMessage(dynamic e) {
-    if (e is FirebaseException) {
-      switch (e.code) {
-        case 'network-request-failed':
-          return S.current.networkRequestFailed;
-        case 'wrong-password':
-          return S.current.wrongPassword;
-        case 'invalid-credential':
-          return S.current.invalidCredential;
-        case 'invalid-email':
-          return S.current.invalidEmail;
-        case 'user-disabled':
-          return S.current.userDisabled;
-        case 'user-not-found':
-          return S.current.userNotFound;
-        case 'email-already-in-use':
-          return S.current.emailAlreadyInUse;
-        case 'operation-not-allowed':
-          return S.current.operationNotAllowed;
-        case 'weak-password':
-          return S.current.weakPassword;
-        case 'too-many-requests':
-          return S.current.tooManyRequests;
-        default:
-          print('----------------FirebaseException: ${e.code}');
-          return e.message ?? S.current.unknownError;
-      }
-    } else {
-      return e.toString();
-    }
-  }
 }
