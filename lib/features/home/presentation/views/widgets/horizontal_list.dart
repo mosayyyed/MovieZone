d0e44c1b8332ab739b2c0ecbf8790358387af0f9
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2025-03-05 03:21:59 +0200

    refactor: enhance styling of horizontal list items with rounded corners

--- lib/features/home/presentation/views/widgets/horizontal_list.dart
+++ lib/features/home/presentation/views/widgets/horizontal_list.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/core/utils/constants.dart';
 import 'package:movie_app/features/home/data/models/movie_model.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 import '../../../data/models/genre_model.dart';
 import '../../controller/genres/genres_cubit.dart';
@@ -59,23 +61,33 @@ class HorizontalList extends StatelessWidget {
           return Padding(
             padding: const EdgeInsets.all(8.0),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
                   width: 150,
                   height: 200,
-                  color: Colors.grey[800],
+                  decoration: BoxDecoration(
+                    color: Colors.grey[800],
+                    borderRadius: BorderRadius.circular(kBorderRadius),
+                  ),
                 ),
                 const SizedBox(height: 8),
                 Container(
                   width: 100,
                   height: 15,
-                  color: Colors.grey[800],
+                  decoration: BoxDecoration(
+                    color: Colors.grey[800],
+                    borderRadius: BorderRadius.circular(kBorderRadius),
+                  ),
                 ),
                 const SizedBox(height: 4),
                 Container(
                   width: 50,
                   height: 15,
-                  color: Colors.grey[800],
+                  decoration: BoxDecoration(
+                    color: Colors.grey[800],
+                    borderRadius: BorderRadius.circular(kBorderRadius),
+                  ),
                 ),
               ],
             ),
