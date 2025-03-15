a14b7857808346da25bfd752809504f1d6e1796d
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2025-03-15 03:35:43 +0200

    feat: refactor theme colors and structure for improved light and dark mode support

--- lib/core/themes/app_colors.dart
+++ lib/core/themes/app_colors.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const kPrimaryColor = Color(0xFF00CF71);
-  static const kPrimaryDarkColor = Color(0xFF0A3922);
-  static const kFillColor = Color(0xFF232429);
-  static const kGreyColor = Color(0xFF8A8A8A);
-  static const kSecondaryColor = Color(0xFF1A1A1A);
-  static const kErrorColor = Color(0xFFFF453A);
-  static const kWarningColor = Color(0xFFFFCC00);
-  static const kInfoColor = Color(0xFF0063F7);
-  static const kSuccessColor = Color(0xFF06C270);
+  static const primaryColor = Color(0xFF00CF71);
+  static const secondaryColor = Color(0xFF98E7C3);
+  static const lightScaffoldBackgroundColor = Color(0xFFFAFAFA);
+  static const darkScaffoldBackgroundColor = Color(0xFF121212);
+
+  static const darkOnPrimary = Color(0xFF121212);
+  static const darkSurface = Color(0xFF2D2D2D);
+  static const darkOnSurface = Color(0xFFFAFAFA);
+
+  static const lightOnPrimary = Color(0xFFFAFAFA);
+  static const lightSurface = Color(0xFFE5FFF4);
+  static const lightOnSurface = Color(0xFF121212);
 }
--- lib/core/themes/app_themes.dart
+++ lib/core/themes/app_themes.dart
@@ -1,15 +1,54 @@
 import 'package:flutter/material.dart';
 
-class AppThemes {
-  static final ThemeData lightTheme = ThemeData(
-    brightness: Brightness.light,
-    primarySwatch: Colors.blue,
-    scaffoldBackgroundColor: Colors.white,
-  );
+import 'app_colors.dart';
 
-  static final ThemeData darkTheme = ThemeData(
-    brightness: Brightness.dark,
-    primarySwatch: Colors.blueGrey,
-    scaffoldBackgroundColor: Colors.black,
-  );
+class AppTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+      brightness: Brightness.light,
+      primaryColor: AppColors.primaryColor,
+      primaryColorLight: AppColors.primaryColor,
+      primaryColorDark: AppColors.primaryColor,
+      scaffoldBackgroundColor: AppColors.lightScaffoldBackgroundColor,
+      useMaterial3: true,
+      colorScheme: const ColorScheme.light(
+        primary: AppColors.primaryColor,
+        onPrimary: AppColors.lightOnPrimary,
+        secondary: AppColors.secondaryColor,
+        onSecondary: AppColors.primaryColor,
+        surface: AppColors.lightSurface,
+        onSurface: AppColors.lightOnSurface,
+        onPrimaryFixed: Colors.grey,
+      ),
+      fontFamily: 'Cairo',
+      appBarTheme: AppBarTheme(
+        backgroundColor: AppColors.primaryColor,
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          fontFamily: 'Cairo',
+        ),
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
+    );
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData(
+      brightness: Brightness.dark,
+      primaryColor: AppColors.primaryColor,
+      scaffoldBackgroundColor: AppColors.darkScaffoldBackgroundColor,
+      colorScheme: const ColorScheme.dark(
+        primary: AppColors.primaryColor,
+        onPrimary: AppColors.darkOnPrimary,
+        secondary: AppColors.secondaryColor,
+        onSecondary: AppColors.primaryColor,
+        surface: AppColors.darkSurface,
+        onSurface: AppColors.darkOnSurface,
+        onPrimaryFixed: Colors.grey,
+      ),
+      fontFamily: 'Cairo',
+    );
+  }
 }
