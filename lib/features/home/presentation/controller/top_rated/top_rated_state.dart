9fbb8342044afc58fcf22d123a0745d36d772cd7
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2025-03-17 02:31:12 +0200

    feat: extend TopRatedState with Equatable for improved state comparison

--- lib/features/home/presentation/controller/top_rated/top_rated_state.dart
+++ lib/features/home/presentation/controller/top_rated/top_rated_state.dart
@@ -1,23 +1,37 @@
 part of 'top_rated_cubit.dart';
 
-sealed class TopRatedState {}
+sealed class TopRatedState extends Equatable {
+  const TopRatedState();
+
+  @override
+  List<Object> get props => [];
+}
 
 final class TopRatedInitial extends TopRatedState {}
 
 final class TopRatedLoading extends TopRatedState {
   final List<MovieModel> fakeTopRatedMovies;
 
-  TopRatedLoading(this.fakeTopRatedMovies);
+  const TopRatedLoading(this.fakeTopRatedMovies);
+
+  @override
+  List<Object> get props => [fakeTopRatedMovies];
 }
 
 final class TopRatedSuccess extends TopRatedState {
   final List<MovieModel> topRatedMovies;
 
-  TopRatedSuccess(this.topRatedMovies);
+  const TopRatedSuccess(this.topRatedMovies);
+
+  @override
+  List<Object> get props => [topRatedMovies];
 }
 
 final class TopRatedError extends TopRatedState {
   final String message;
 
-  TopRatedError(this.message);
+  const TopRatedError(this.message);
+
+  @override
+  List<Object> get props => [message];
 }
