a959bc344c3caedd73b747bc4dc34be3238c755e
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2025-03-20 21:26:51 +0200

    refactor: move app initialization to MovieZoneApp for improved structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:movie_app/core/themes/theme_manager.dart';
+import 'package:movie_app/movie_zone_app.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'core/routing/routes.dart';
-import 'core/themes/app_themes.dart';
 import 'core/utils/service_locator.dart';
 import 'firebase_options.dart';
-import 'generated/l10n.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,43 +15,7 @@ void main() async {
   );
 
   setupServiceLocator();
-  runApp(MyApp(
+  runApp(MovieZoneApp(
     prefs: prefs,
   ));
-}
-
-class MyApp extends StatelessWidget {
-  final SharedPreferences prefs;
-  const MyApp({super.key, required this.prefs});
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (_) => ThemeCubit(prefs)),
-      ],
-      child: BlocBuilder<ThemeCubit, ThemeState>(
-        builder: (context, state) {
-          final themeMode =
-              state is ThemeChanged ? state.themeMode : ThemeMode.system;
-          return MaterialApp.router(
-            debugShowCheckedModeBanner: false,
-            locale: const Locale('ar'),
-            localizationsDelegates: [
-              S.delegate,
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
-            ],
-            supportedLocales: S.delegate.supportedLocales,
-            title: 'Movie App',
-            theme: AppTheme.lightTheme,
-            darkTheme: AppTheme.darkTheme,
-            themeMode: themeMode,
-            routerConfig: AppRoutes.router,
-          );
-        },
-      ),
-    );
-  }
 }
---
+++ lib/movie_zone_app.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:movie_app/core/routing/routes.dart';
+import 'package:movie_app/core/themes/app_themes.dart';
+import 'package:movie_app/core/themes/theme_manager.dart';
+import 'package:movie_app/generated/l10n.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class MovieZoneApp extends StatelessWidget {
+  final SharedPreferences prefs;
+  const MovieZoneApp({super.key, required this.prefs});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => ThemeCubit(prefs)),
+      ],
+      child: BlocBuilder<ThemeCubit, ThemeState>(
+        builder: (context, state) {
+          final themeMode =
+              state is ThemeChanged ? state.themeMode : ThemeMode.system;
+          return MaterialApp.router(
+            debugShowCheckedModeBanner: false,
+            locale: const Locale('ar'),
+            localizationsDelegates: [
+              S.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: S.delegate.supportedLocales,
+            title: 'Movie App',
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            themeMode: themeMode,
+            routerConfig: AppRoutes.router,
+          );
+        },
+      ),
+    );
+  }
+}
