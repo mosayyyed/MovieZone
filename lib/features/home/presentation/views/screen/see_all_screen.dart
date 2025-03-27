2bbb330871897fd7accd57f9edae2d20df87e1bf
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2025-03-27 23:17:02 +0200

    feat: add title parameter to SeeAllScreen and update layout for improved UI

--- lib/features/home/presentation/views/screen/see_all_screen.dart
+++ lib/features/home/presentation/views/screen/see_all_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:movie_app/core/themes/app_values.dart';
 import 'package:movie_app/features/explore/presentation/views/screen/explore_screen.dart';
 import 'package:movie_app/features/home/data/models/movie_model.dart';
 import 'package:movie_app/features/home/presentation/controller/genres/genres_cubit.dart';
@@ -8,7 +9,8 @@ import 'package:movie_app/features/onboarding/presentation/widgets/skip_button.d
 
 class SeeAllScreen extends StatelessWidget {
   final List<MovieModel> movies;
-  const SeeAllScreen({super.key, required this.movies});
+  final String title;
+  const SeeAllScreen({super.key, required this.movies, required this.title});
 
   @override
   Widget build(BuildContext context) {
@@ -22,18 +24,35 @@ class SeeAllScreen extends StatelessWidget {
         titleSpacing: 0,
         title: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 18),
-          child: SkipButton(
-            icon: Icon(
-              Icons.arrow_back_ios_new_rounded,
-              color: Theme.of(context).colorScheme.onSurface,
-              size: 22,
-            ),
-            hasBorderSide: true,
-            onPressed: () => GoRouter.of(context).pop(),
+          child: Row(
+            children: [
+              SkipButton(
+                icon: Icon(
+                  Icons.arrow_back_ios_new_rounded,
+                  color: Theme.of(context).colorScheme.onSurface,
+                  size: 22,
+                ),
+                hasBorderSide: true,
+                onPressed: () => GoRouter.of(context).pop(),
+              ),
+              const SizedBox(width: AppSize.s8),
+              Text(
+                title,
+                style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface,
+                      fontSize: AppSize.s20,
+                      fontWeight: FontWeight.w600,
+                    ),
+              ),
+            ],
           ),
         ),
       ),
-      body: MoviesGridView(genresCubit: genresCubit, movies: movies),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+            horizontal: AppPadding.p16, vertical: 4.0),
+        child: MoviesGridView(genresCubit: genresCubit, movies: movies),
+      ),
     );
   }
 }
