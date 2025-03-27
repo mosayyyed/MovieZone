8462da9ef818c3aa153d24b322d9ba36dcd4cd39
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2025-03-27 23:18:50 +0200

    refactor: simplify InputDecoration by removing unused styles and borders

--- lib/core/utils/build_input_decoration.dart
+++ lib/core/utils/build_input_decoration.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../themes/app_styles.dart';
-import 'constants.dart';
-
 InputDecoration buildInputDecoration({
   String? hintText,
   String? labelText,
@@ -12,50 +9,9 @@ InputDecoration buildInputDecoration({
   required BuildContext context,
   VoidCallback? onSuffixIconPressed,
 }) {
-  final OutlineInputBorder border = OutlineInputBorder(
-    borderRadius: BorderRadius.circular(kBorderRadius),
-    borderSide: BorderSide.none,
-  );
-
-  final OutlineInputBorder focusedBorder = OutlineInputBorder(
-    borderRadius: BorderRadius.circular(kBorderRadius),
-    borderSide: BorderSide(
-      color: Theme.of(context).colorScheme.primary,
-      width: 1.5,
-    ),
-  );
-
-  final OutlineInputBorder errorBorder = OutlineInputBorder(
-    borderRadius: BorderRadius.circular(kBorderRadius),
-    borderSide: BorderSide(
-      color: Theme.of(context).colorScheme.error,
-      width: 1.5,
-    ),
-  );
-
   return InputDecoration(
     hintText: hintText,
-    hintStyle: Styles.textStyle14
-        .copyWith(color: Theme.of(context).colorScheme.secondary),
-    label: Text(
-      labelText ?? "",
-      style: Styles.textStyle12.copyWith(
-          color: Theme.of(context).colorScheme.onSurface,
-          fontWeight: FontWeight.bold),
-    ),
-    errorMaxLines: 2,
-    errorStyle: TextStyle(
-      color: Theme.of(context).colorScheme.error.withAlpha(180),
-      fontSize: 12,
-      overflow: TextOverflow.ellipsis,
-    ),
-    filled: true,
-    fillColor: Theme.of(context).colorScheme.surface,
-    border: border,
-    enabledBorder: border,
-    focusedBorder: focusedBorder,
-    focusedErrorBorder: errorBorder,
-    errorBorder: errorBorder,
+    label: Text(labelText ?? ""),
     prefixIcon: prefixIcon,
     suffixIcon: suffixIconVisibility
         ? IconButton(
