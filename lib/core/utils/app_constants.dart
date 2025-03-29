c1853ef22d9447e4fb840fb2ca74e40144ee90e1
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2025-03-30 01:16:32 +0200

    feat: integrate dotenv for API key management in AppConstants

--- lib/core/utils/app_constants.dart
+++ lib/core/utils/app_constants.dart
@@ -1,14 +1,4 @@
-//border radius
-const kBorderRadius = 12.0;
-
-//api base url
-// const kBaseUrl = "https://api.themoviedb.org/3";
-
-//api image base url
-// String kFullImageUrl = "https://image.tmdb.org/t/p/original";
-
-//api key
-const kApiKey = "d6ae02fa2806b362ce91ce9b710bd2c5";
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppConstants {
   static const api = _ApiConstants();
@@ -21,7 +11,7 @@ class _ApiConstants {
 
   String get baseUrl => "https://api.themoviedb.org/3";
   String get imageUrl => "https://image.tmdb.org/t/p/original";
-  String get apiKey => "d6ae02fa2806b362ce91ce9b710bd2c5";
+  String get apiKey => dotenv.env["API_KEY"] ?? '';
 }
 
 class _SharedPreferencesConstants {
