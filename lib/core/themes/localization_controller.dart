b769588ea798be04b07a21255952c059e87e619d
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2025-03-30 01:18:24 +0200

    feat: add LocalizationCubit for managing app language preferences

---
+++ lib/core/themes/localization_controller.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LocalizationCubit extends Cubit<Locale> {
+  final SharedPreferences _prefs;
+
+  static const String _languageKey = 'selected_language';
+
+  LocalizationCubit(this._prefs) : super(const Locale('en')) {
+    _loadSavedLanguage();
+  }
+
+  Future<void> _loadSavedLanguage() async {
+    final languageCode = _prefs.getString(_languageKey) ?? 'en';
+    emit(Locale(languageCode));
+  }
+
+  Future<void> changeLanguage(String languageCode) async {
+    await _prefs.setString(_languageKey, languageCode);
+    emit(Locale(languageCode));
+  }
+}
