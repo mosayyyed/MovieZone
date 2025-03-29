6b9755598d65148d572423674ae61eed8d1a7490
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2025-03-29 23:30:01 +0200

    fix: remove SharedPreferences dependency from main and refactor ThemeCubit initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_app/movie_zone_app.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import 'core/utils/service_locator.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final prefs = await SharedPreferences.getInstance();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
   setupServiceLocator();
-  runApp(MovieZoneApp(
-    prefs: prefs,
-  ));
+  runApp(const MovieZoneApp());
 }
--- lib/movie_zone_app.dart
+++ lib/movie_zone_app.dart
@@ -3,27 +3,32 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:movie_app/core/routing/routes.dart';
 import 'package:movie_app/core/themes/app_themes.dart';
+import 'package:movie_app/core/themes/localization_controller.dart';
 import 'package:movie_app/core/themes/theme_controller.dart';
+import 'package:movie_app/core/utils/service_locator.dart';
 import 'package:movie_app/generated/l10n.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class MovieZoneApp extends StatelessWidget {
-  final SharedPreferences prefs;
-  const MovieZoneApp({super.key, required this.prefs});
+  const MovieZoneApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (_) => ThemeCubit(prefs)),
+        BlocProvider<ThemeCubit>(
+            create: (_) => ThemeCubit(getIt.get<SharedPreferences>())),
       ],
-      child: BlocBuilder<ThemeCubit, ThemeState>(
-        builder: (context, state) {
-          final themeMode =
-              state is ThemeChanged ? state.themeMode : ThemeMode.system;
+      child: Builder(
+        builder: (context) {
+          final themeState = context.watch<ThemeCubit>().state;
+          final themeMode = themeState is ThemeChanged
+              ? themeState.themeMode
+              : ThemeMode.system;
+
           return MaterialApp.router(
             debugShowCheckedModeBanner: false,
-            locale: const Locale('ar'),
+            locale: Locale("ar"),
             localizationsDelegates: [
               S.delegate,
               GlobalMaterialLocalizations.delegate,
