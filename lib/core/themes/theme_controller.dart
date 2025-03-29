be41c514456bf46b998e894a4093c92958409299
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2025-03-30 01:18:45 +0200

    fix: refactor ThemeCubit to use AppConstants for shared preferences key

--- lib/core/themes/theme_controller.dart
+++ lib/core/themes/theme_controller.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/core/utils/app_constants.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeCubit extends Cubit<ThemeState> {
-  final SharedPreferences prefs;
+  final SharedPreferences _prefs;
 
-  ThemeCubit(this.prefs) : super(ThemeInitial()) {
+  ThemeCubit(this._prefs) : super(ThemeInitial()) {
     loadTheme();
   }
 
-  static const String _themeKey = "theme_mode";
-
   void loadTheme() {
-    final themeIndex = prefs.getInt(_themeKey) ?? 0;
+    final themeIndex =
+        _prefs.getInt(AppConstants.sharedPreferences.themeKey) ?? 0;
     emit(ThemeChanged(ThemeMode.values[themeIndex]));
   }
 
   Future<void> toggleTheme(ThemeMode themeMode) async {
-    await prefs.setInt(_themeKey, themeMode.index);
+    await _prefs.setInt(
+        AppConstants.sharedPreferences.themeKey, themeMode.index);
     emit(ThemeChanged(themeMode));
   }
 }
