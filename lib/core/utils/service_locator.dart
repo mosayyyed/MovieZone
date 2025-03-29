7f9140e58d8c35f4592e2056af4eadf3928e2560
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2025-03-30 01:19:41 +0200

    feat: update service locator to register SharedPreferences for improved state management

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 import 'package:movie_app/features/auth/data/repositories/auth_repo_impl.dart';
 import 'package:movie_app/features/home/data/repositories/movie_repo/movie_repo_impl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../features/explore/data/repositories/search_repo/search_movies_repo_impl.dart';
 import '../../features/home/data/repositories/movie_details_repo/movie_details_repo_impl.dart';
@@ -11,7 +12,10 @@ import '../data_sources/remote/firebase_service.dart';
 
 final GetIt getIt = GetIt.instance;
 
-void setupServiceLocator() {
+void setupServiceLocator() async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  getIt.registerSingleton<SharedPreferences>(prefs);
   getIt.registerSingleton<AuthRepoImpl>(AuthRepoImpl(
       FirebaseService(FirebaseAuth.instance, FirebaseFirestore.instance)));
   getIt.registerSingleton<MovieRepoImpl>(MovieRepoImpl(HttpApiService()));
