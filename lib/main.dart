1e649d49dc4fccd7eb8ae0db07ac58193aacc8dd
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2025-03-30 01:18:08 +0200

    feat: load environment variables using dotenv in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:movie_app/movie_zone_app.dart';
 
 import 'core/utils/service_locator.dart';
@@ -11,6 +12,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  dotenv.load(fileName: ".env");
 
   setupServiceLocator();
   runApp(const MovieZoneApp());
