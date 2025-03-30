1b4ba8e3071d6ffda87b9506f0a5b4fb83833d3e
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2025-03-30 14:47:10 +0200

    refactor: improve theme loading and toggling logic in ThemeCubit

--- lib/core/controllers/theme_controller.dart
+++ lib/core/controllers/theme_controller.dart
@@ -4,19 +4,22 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeCubit extends Cubit<ThemeMode> {
   final SharedPreferences _prefs;
+
   static const String _themeKey = 'selected_theme';
 
   ThemeCubit(this._prefs) : super(ThemeMode.system) {
-    loadTheme();
+    _loadTheme();
   }
 
-  void loadTheme() {
+  Future<void> _loadTheme() async {
     final themeIndex = _prefs.getInt(_themeKey) ?? 0;
     emit(ThemeMode.values[themeIndex]);
   }
 
   Future<void> toggleTheme(ThemeMode themeMode) async {
-    await _prefs.setInt(_themeKey, themeMode.index);
-    emit(themeMode);
+    if (state != themeMode) {
+      await _prefs.setInt(_themeKey, themeMode.index);
+      emit(themeMode);
+    }
   }
 }
