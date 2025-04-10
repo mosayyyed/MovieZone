fbafa315361a306ace860b2244a7ce37f2396171
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2025-04-10 02:49:26 +0200

    refactor: update AccountStatusIndicator to use IntrinsicWidth for better layout handling and maintain minimum width

--- lib/features/profile/presentation/views/ui/account_status_indicator.dart
+++ lib/features/profile/presentation/views/ui/account_status_indicator.dart
@@ -6,22 +6,28 @@ class AccountStatusIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.secondary,
-        borderRadius: BorderRadius.circular(50),
-      ),
-      child: Row(
-        children: [
-          Icon(Icons.circle,
-              color: Theme.of(context).colorScheme.primary, size: 14),
-          const SizedBox(width: 4),
-          Text("نشط",
+    return IntrinsicWidth(
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.secondary,
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(Icons.circle,
+                color: Theme.of(context).colorScheme.primary, size: 14),
+            const SizedBox(width: 4),
+            Text(
+              "نشط",
               style: Styles.textStyle12.copyWith(
-                  fontWeight: FontWeight.bold,
-                  color: Theme.of(context).colorScheme.primary)),
-        ],
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
