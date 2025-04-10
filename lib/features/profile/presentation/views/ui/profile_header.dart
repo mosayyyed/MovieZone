164e285f20fc3db23eac149dcae68e494050f32a
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2025-04-10 02:47:05 +0200

    feat: implement ProfileContent widget to encapsulate user profile display logic and enhance ProfileHeader with loading state handling

--- lib/features/profile/presentation/views/ui/profile_header.dart
+++ lib/features/profile/presentation/views/ui/profile_header.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:movie_app/core/themes/app_styles.dart';
 import 'package:movie_app/features/profile/presentation/controller/cubit/user_cubit_cubit.dart';
 import 'package:movie_app/features/profile/presentation/controller/cubit/user_cubit_state.dart';
-import 'package:movie_app/features/profile/presentation/views/ui/account_status_indicator.dart';
+import 'package:movie_app/features/profile/presentation/views/ui/profile_content.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class ProfileHeader extends StatelessWidget {
   const ProfileHeader({super.key});
@@ -13,96 +13,15 @@ class ProfileHeader extends StatelessWidget {
     return BlocBuilder<UserCubit, UserState>(
       builder: (context, state) {
         if (state is UserLoading) {
-          return const Center(child: CircularProgressIndicator());
+          return Skeletonizer(
+            enabled: true,
+            child: ProfileContent(
+              user: state.user,
+            ),
+          );
         } else if (state is UserLoaded) {
-          final user = state.user;
-          return Row(
-            children: [
-              Stack(
-                children: [
-                  InkWell(
-                    onTap: () {},
-                    borderRadius: BorderRadius.circular(10),
-                    child: Container(
-                      width: 120,
-                      height: 160,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Theme.of(context).colorScheme.secondary,
-                        image: DecorationImage(
-                          image:
-                              AssetImage("assets/avatar.png") as ImageProvider,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    top: 4,
-                    right: 4,
-                    child: GestureDetector(
-                      onTap: () {},
-                      child: CircleAvatar(
-                        backgroundColor: Theme.of(context).colorScheme.surface,
-                        radius: 16,
-                        child: Icon(
-                          Icons.camera_alt_rounded,
-                          color: Theme.of(context).colorScheme.onSurface,
-                          size: 18,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(width: 12),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    user.fullName,
-                    style: Styles.boldTextStyle18
-                        .copyWith(fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 4),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.45,
-                    child: Text(
-                      user.email.split('@').first,
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
-                      style: Styles.textStyle14.copyWith(
-                        color: Theme.of(context).colorScheme.onSurfaceVariant,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  AccountStatusIndicator(),
-                  const SizedBox(height: 8),
-                  ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: const Icon(Icons.edit, size: 18),
-                    label: Text(
-                      "تعديل الحساب",
-                      style: Styles.textStyle14
-                          .copyWith(fontWeight: FontWeight.bold),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      elevation: 0,
-                      minimumSize: const Size(140, 30),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 16, vertical: 8),
-                      textStyle: Theme.of(context)
-                          .textTheme
-                          .bodyMedium
-                          ?.copyWith(fontWeight: FontWeight.bold),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8)),
-                    ),
-                  ),
-                ],
-              ),
-            ],
+          return ProfileContent(
+            user: state.user,
           );
         } else if (state is UserError) {
           return Center(child: Text(state.message));
