35055798508a78988f78f5cdee2f394f3bb16c07
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2025-04-10 02:54:30 +0200

    feat: implement UserLocalStorageService for managing user data with SharedPreferences

--- lib/core/data_sources/local/local_storage_service.dart
+++ lib/core/data_sources/local/local_storage_service.dart
@@ -1,6 +1,32 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 abstract class LocalStorageService {
   Future<void> saveData(String key, dynamic value);
   dynamic getData(String key, {dynamic defaultValue});
   Future<void> deleteData(String key);
   Future<void> clearAll();
+}
+
+class UserLocalStorageService extends LocalStorageService {
+  @override
+  Future<void> saveData(String key, dynamic value) async {
+    await SharedPreferences.getInstance()
+        .then((value) => value.setString(key, value.toString()));
+  }
+
+  @override
+  dynamic getData(String key, {dynamic defaultValue}) {
+    return SharedPreferences.getInstance()
+        .then((value) => value.getString(key));
+  }
+
+  @override
+  Future<void> deleteData(String key) async {
+    await SharedPreferences.getInstance().then((value) => value.remove(key));
+  }
+
+  @override
+  Future<void> clearAll() async {
+    await SharedPreferences.getInstance().then((value) => value.clear());
+  }
 }
