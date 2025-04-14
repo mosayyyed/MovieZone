edb70e6fe798df9a1220b2b07f5d461f0b94e9a7
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2025-04-14 18:37:12 +0200

    feat: Add ArrowBackWithTitleAppBar for enhanced navigation
    - Introduced ArrowBackWithTitleAppBar widget to provide a consistent back navigation experience with a title.
    - Implemented a transparent AppBar with a back button and centralized title styling, improving UI consistency.
    - Integrated GoRouter for back navigation functionality, enhancing user experience across the application.

---
+++ lib/src/core/ui/arrow_back_with_title_app_bar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:movie_app/src/core/themes/app_styles.dart';
+import 'package:movie_app/src/core/themes/app_values.dart';
+
+class ArrowBackWithTitleAppBar extends StatelessWidget
+    implements PreferredSizeWidget {
+  final String title;
+
+  const ArrowBackWithTitleAppBar({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      scrolledUnderElevation: 0,
+      automaticallyImplyLeading: false,
+      centerTitle: true,
+      leading: Padding(
+        padding: const EdgeInsetsDirectional.only(start: AppPadding.p8),
+        child: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios_new_rounded,
+            color: Theme.of(context).colorScheme.onSurface,
+            size: 22,
+          ),
+          onPressed: () => GoRouter.of(context).pop(),
+          splashRadius: 24,
+        ),
+      ),
+      title: Text(
+        title,
+        style: Styles.boldTextStyle20.copyWith(
+          color: Theme.of(context).colorScheme.onSurface,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
