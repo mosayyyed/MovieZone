28ed6a292d3e8de49e4998e81053872695c563ae
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2025-04-14 15:12:37 +0200

    refactor: Enhance ProgressBarWithPercentage with blur effect and dynamic width calculation
    - Updated ProgressBarWithPercentage to use a LayoutBuilder for dynamic width calculation based on progress.
    - Introduced a BackdropFilter for a blurred background effect, improving the visual appearance of the progress bar.
    - Refactored the progress bar's container to enhance UI consistency and responsiveness.

--- lib/src/core/ui/progress_bar_with_percentage.dart
+++ lib/src/core/ui/progress_bar_with_percentage.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:movie_app/src/core/themes/app_values.dart';
 
@@ -21,33 +22,40 @@ class ProgressBarWithPercentage extends StatelessWidget {
               fontWeight: FontWeight.bold,
               shadows: [
                 Shadow(
-                    offset: Offset(0, 1), blurRadius: 2, color: Colors.black45)
+                  offset: Offset(0, 1),
+                  blurRadius: 2,
+                  color: Colors.black45,
+                ),
               ],
             ),
           ),
         ),
-        Stack(
-          children: [
-            Container(
-              height: 4,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.2),
-                borderRadius: BorderRadius.circular(8),
-              ),
-            ),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: LinearProgressIndicator(
-                value: progress,
-                minHeight: 4,
-                backgroundColor: Colors.transparent,
-                valueColor: AlwaysStoppedAnimation<Color>(
-                  Theme.of(context).colorScheme.primary,
-                ),
-              ),
-            ),
-          ],
+        ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              final fullWidth = constraints.maxWidth;
+              final progressWidth = fullWidth * progress.clamp(0.0, 1.0);
+
+              return Stack(
+                children: [
+                  BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 4, sigmaY: 4),
+                    child: Container(
+                      height: 4,
+                      width: fullWidth,
+                      color: Colors.white.withOpacity(0.1),
+                    ),
+                  ),
+                  Container(
+                    height: 4,
+                    width: progressWidth,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ],
+              );
+            },
+          ),
         ),
       ],
     );
