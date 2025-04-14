e1bcf9ba0079e63a53c5f8a2e637d084828ead20
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2025-04-14 20:18:51 +0200

    refactor: Simplify app bar in SeeAllScreen with ArrowBackWithTitleAppBar
    - Replaced the custom AppBar implementation with ArrowBackWithTitleAppBar for improved navigation consistency.
    - Removed unnecessary imports and streamlined the code for better readability.

--- lib/src/features/home/presentation/views/screen/see_all_screen.dart
+++ lib/src/features/home/presentation/views/screen/see_all_screen.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
-import 'package:movie_app/src/core/themes/app_styles.dart';
 import 'package:movie_app/src/core/themes/app_values.dart';
+import 'package:movie_app/src/core/ui/arrow_back_with_title_app_bar.dart';
 import 'package:movie_app/src/features/explore/presentation/views/ui/explore/movies_grid_view.dart';
 import 'package:movie_app/src/features/home/data/models/movie_model.dart';
 import 'package:movie_app/src/features/home/presentation/controller/genres/genres_cubit.dart';
-import 'package:movie_app/src/features/onboarding/presentation/ui/skip_button.dart';
 
 class SeeAllScreen extends StatelessWidget {
   final List<MovieModel> movies;
@@ -17,34 +15,7 @@ class SeeAllScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final genresCubit = context.watch<GenresCubit>();
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        scrolledUnderElevation: 0,
-        automaticallyImplyLeading: false,
-        titleSpacing: 0,
-        title: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 18),
-          child: Row(
-            children: [
-              SkipButton(
-                icon: Icon(
-                  Icons.arrow_back_ios_new_rounded,
-                  color: Theme.of(context).colorScheme.onSurface,
-                  size: 22,
-                ),
-                hasBorderSide: true,
-                onPressed: () => GoRouter.of(context).pop(),
-              ),
-              const SizedBox(width: AppSize.s8),
-              Text(
-                title,
-                style: Styles.boldTextStyle20,
-              ),
-            ],
-          ),
-        ),
-      ),
+      appBar: ArrowBackWithTitleAppBar(title: title),
       body: Padding(
         padding: const EdgeInsets.symmetric(
             horizontal: AppPadding.p16, vertical: 4.0),
