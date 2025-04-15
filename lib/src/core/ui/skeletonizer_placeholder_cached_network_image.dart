9c3c58653f446f8c2931b000c287eab059fe37ee
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2025-04-15 05:00:55 +0200

    feat: Add customizable dimensions to SkeletonizerPlaceholderCachedNetworkImage
    - Introduced optional width and height parameters to the SkeletonizerPlaceholderCachedNetworkImage widget, allowing for more flexible layout options.
    - Updated the container's color to use the theme's surface container color for improved visual consistency.

--- lib/src/core/ui/skeletonizer_placeholder_cached_network_image.dart
+++ lib/src/core/ui/skeletonizer_placeholder_cached_network_image.dart
@@ -2,19 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
 class SkeletonizerPlaceholderCachedNetworkImage extends StatelessWidget {
+  final double? width;
+  final double? height;
+
   const SkeletonizerPlaceholderCachedNetworkImage({
     super.key,
+    this.width,
+    this.height,
   });
 
   @override
   Widget build(BuildContext context) {
     return Skeletonizer(
-        enabled: true,
-        child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          color: Colors.white,
-        ));
+      enabled: true,
+      child: Container(
+        width: width ?? double.infinity,
+        height: height ?? double.infinity,
+        color: Theme.of(context).colorScheme.surfaceContainerHighest,
+      ),
+    );
   }
 }
-
