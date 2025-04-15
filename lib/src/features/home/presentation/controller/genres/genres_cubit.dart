95636b0e0ac6abe60449e0fdc7bbbefb60534d7d
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2025-04-15 04:59:39 +0200

    refactor: Improve genre fetching logic in GenresCubit
    - Updated GenresCubit to automatically fetch genres upon initialization if the genres list is empty.
    - Enhanced error handling in fetchGenres method to catch exceptions and emit appropriate error states.
    - Cleaned up code for better readability and maintainability.

--- lib/src/features/home/presentation/controller/genres/genres_cubit.dart
+++ lib/src/features/home/presentation/controller/genres/genres_cubit.dart
@@ -1,28 +1,34 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
-import '../../../data/models/genre_model.dart';
-import '../../../data/repositories/movie_repo/movie_repo.dart';
+import 'package:movie_app/src/features/home/data/models/genre_model.dart';
+import 'package:movie_app/src/features/home/data/repositories/movie_repo/movie_repo.dart';
 
 part 'genres_state.dart';
 
 class GenresCubit extends Cubit<GenresState> {
   final MovieRepo movieRepo;
-
   final List<GenreModel> genres = [];
-  GenresCubit(this.movieRepo) : super(GenresInitial());
 
-  void fetchGenres() async {
-    emit(GenresLoading());
-    final response = await movieRepo.fetchGenres();
-    response.fold(
-      (failure) => emit(GenresError(failure.message)),
-      (genres) {
-        this.genres.clear();
-        this.genres.addAll(genres);
-        emit(GenresSuccess(genres));
-      },
-    );
+  GenresCubit(this.movieRepo) : super(GenresInitial()) {
+    if (genres.isEmpty) {
+      fetchGenres();
+    }
+  }
+
+  Future<void> fetchGenres() async {
+    try {
+      emit(GenresLoading());
+      final response = await movieRepo.fetchGenres();
+      response.fold(
+        (failure) => emit(GenresError(failure.message)),
+        (genresList) {
+          genres.clear();
+          genres.addAll(genresList);
+          emit(GenresSuccess(genres));
+        },
+      );
+    } catch (e) {
+      emit(GenresError(e.toString()));
+    }
   }
 }
