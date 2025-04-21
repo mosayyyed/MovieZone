ce0faa59302b95ab296d34fad0eb1f9041fe5c4e
Mohamed Elsayed <113109457+mosayyyed@users.noreply.github.com>
2025-04-21 02:40:21 +0200

    chore: remove device_preview package from dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:movie_app/src/movie_zone_app.dart';
-import 'package:device_preview/device_preview.dart';
 
 import 'src/core/utils/service_locator.dart';
 import 'firebase_options.dart';
@@ -11,10 +10,7 @@ void main() async {
   await _initializeServices();
 
   runApp(
-    DevicePreview(
-      enabled: true,
-      builder: (context) => const MovieZoneApp(),
-    ),
+    const MovieZoneApp(),
   );
 }
 
